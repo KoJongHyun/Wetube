54fec17720eb219f7e6576be2b1556d838c10a28
KoJongHyun <no1kojong@gmail.com>
2019-06-17 23:04:08 +0900

    Connecting MongoDB Configuring Dot Env Modelling : Video, Comment

--- db.js
+++ db.js
@@ -1,37 +1,15 @@
-export const videos = [{
-    id: 21383,
-    title: 'Video Awesome',
-    description: 'This is something I love',
-    views: 24,
-    videoFile: 'https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4',
-    creator: {
-      id: 93939,
-      name: 'Ko Jonghyun',
-      email: 'no1kojong@gmail.com'
-    }
-  },
-  {
-    id: 55655,
-    title: 'Video Good',
-    description: 'This is something I love',
-    views: 24,
-    videoFile: 'https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4',
-    creator: {
-      id: 93939,
-      name: 'Ko Jonghyun',
-      email: 'no1kojong@gmail.com'
-    }
-  },
-  {
-    id: 67856,
-    title: 'Video Perfect',
-    description: 'This is something I love',
-    views: 24,
-    videoFile: 'https://interactive-examples.mdn.mozilla.net/media/examples/flower.mp4',
-    creator: {
-      id: 93939,
-      name: 'Ko Jonghyun',
-      email: 'no1kojong@gmail.com'
-    }
-  }
-]
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+const handleOpen = () => console.log('Connected DB');
+const handleError = (error) => console.log(`Error on Connection: ${error}`);
+
+db.once('open', handleOpen);
+db.on('error', handleError);
