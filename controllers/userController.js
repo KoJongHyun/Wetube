46223e5efa15d61574cfd6300db89a78d5f5d7ea
KoJongHyun <no1kojong@gmail.com>
2019-07-09 23:36:05 +0900

    Login the User In

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import passport from 'passport';
 import routes from '../routes';
 import User from '../models/User';
 
@@ -27,19 +28,22 @@ export const postJoin = async(req, res) => {
         user,
         password
       );
+      next();
     } catch (error) {
       console.log(error);
+      res.redirect(routes.home);
     }
-    res.redirect(routes.home);
   }
 }
 
 export const getLogin = (req, res) => {
   res.render('login', { pageTitle: 'Login' });
 }
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-}
+export const postLogin = passport.authenticate('local', {
+  successRedirect: routes.home,
+  failureRedirect: routes.login
+});
+
 export const logout = (req, res) => {
   res.redirect(routes.home);
 }
